06638faa700c49ffc7c6badd76007e15affcc75b
Porsche <herskorn@gmail.com>
2024-02-19 12:40:25 -0800

    "added README content"

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# javascript_password_generator
+# Javascript Password Generator
+
+## Decription
+The password generator functions to produce a string of random characters through Javascript code. The character string produced is intentionally limited by the parameters defined in the script code.
+
+The password generator operates through the use of <code>functions</code>, <code>loops</code>, and <code>variables</code> written into the script.
+
+
+## Installation
+To operate the Password Generator, a user must have a browser that supports Javascript and has it enabled.
+
+
+## Usage
+Opening the [Password Generator Webpage](https://eepitsporsche.github.io/javascript_password_generator/) and clicking the "Generate Password" button will produce a randomly generated string of characters. A series of prompts determine the parameters (password length and character types) of the password generated.
+
+If minimum criteria is not met (a password length of between 8-128 characters and at least one character type selected), the user will recieve an alert indicating required criteria has not been met. The user can again click "Generate Password" to begin the process over and meet required criteria.
+
+![Password Generator Screen Capture](./assets/password_generator_screencapture.gif)
+
+
+## Credits
+* [dev.to](https://dev.to/code_mystery/random-password-generator-using-javascript-6a) was used to refernce how to implement a <code>for</code> loop with the <code>Math.random()</code> method.
+
+* [mdn web docs_](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment) was used to understand how to include selected character criteria into the generated password.
+
+* [Code Forum](https://codeforum.org/threads/javascript-password-generator-i-need-help.6275/) was used to understand how to create an alert when character criteria is not met.
+
+* [W3 Schools](https://www.w3schools.com/jsref/jsref_isnan_number.asp) was used to understand the <code>isNaN</code> method.
+
+* UCB Bootcamp provided starter code for HTML structure and CSS styling of the Password Generator webpage.
+
+## License
+N/A
--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,64 @@
+
 //Function to generate a random password
 function generatePassword() {
 
-  //Character type array for computer to choose from (lowercase, uppercase, numeric, special characters)
-    var numericCharacters = "0123456789";
-    var specialCharacters = "!'()%*+,-.&/:;<=>?@[]^$_`{|#}~";
-    var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordCharacters = "";
-    var newPassword = "";
-  
-  
-  //User inputs desired password length
-    var userInput = prompt("Enter a password length from 8 to 128.")
-  
-  
-  //If user input is not a number 8-128
-    if (isNaN(userInput) || userInput < 8 || userInput > 128){
-      alert("Please choose a number from 8 to 128.");
-      return
+//Character type array for computer to choose from (lowercase, uppercase, numeric, special characters)
+  var numericCharacters = "0123456789";
+  var specialCharacters = "!'()%*+,-.&/:;<=>?@[]^$_`{|#}~";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordCharacters = "";
+  var newPassword = "";
+
+
+//User inputs desired password length
+  var userInput = prompt("Enter a password length from 8 to 128.")
+
+
+//If user input is not a number 8-128
+  if (isNaN(userInput) || userInput < 8 || userInput > 128){
+    alert("Please choose a number from 8 to 128.");
+    return
+  }
+
+
+//Prompts to verify character types
+  else {
+    var useNumericCharacters = confirm("Press 'Okay' to include numeric characters or 'Cancel' to omit.");
+    var useSpecialCharacters = confirm("Press 'Okay' to include special characters or 'Cancel' to omit.");
+    var useLowercase = confirm("Press 'Okay' to include lowercase characters or 'Cancel' to omit.");
+    var useUppercase = confirm("Press 'Okay' to include uppercase characters or 'Cancel' to omit.");
     }
-  
-  
-  //Prompts to verify character types
-    else {
-      var useNumericCharacters = confirm("Press 'Okay' to include numeric characters or 'Cancel' to omit.");
-      var useSpecialCharacters = confirm("Press 'Okay' to include special characters or 'Cancel' to omit.");
-      var useLowercase = confirm("Press 'Okay' to include lowercase characters or 'Cancel' to omit.");
-      var useUppercase = confirm("Press 'Okay' to include uppercase characters or 'Cancel' to omit.");
-      }
-  
-      if (useNumericCharacters) {
-        passwordCharacters += numericCharacters
-      }
-      if (useSpecialCharacters) {
-        passwordCharacters += specialCharacters
-      }
-      if (useLowercase) {
-        passwordCharacters += lowercase
-      }
-      if (useUppercase) {
-        passwordCharacters += uppercase
-      }
-  
-  
-  //If user does not select any character types
-    if (useNumericCharacters === false && useSpecialCharacters === false && useLowercase === false && useUppercase === false) {
-      alert("Please choose at least one character type.")
-      return
+
+    if (useNumericCharacters) {
+      passwordCharacters += numericCharacters
     }
-  
-  
-  //for loop generates a random password once all criteria are met
-    for (var i = 0; i < userInput; i++) {
-      newPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-      }
-      return(newPassword)
+    if (useSpecialCharacters) {
+      passwordCharacters += specialCharacters
     }
-  
-  
+    if (useLowercase) {
+      passwordCharacters += lowercase
+    }
+    if (useUppercase) {
+      passwordCharacters += uppercase
+    }
+
+
+//If user does not select any character types
+  if (useNumericCharacters === false && useSpecialCharacters === false && useLowercase === false && useUppercase === false) {
+    alert("Please choose at least one character type.")
+    return
+  }
+
+
+//for loop generates a random password once all criteria are met
+  for (var i = 0; i < userInput; i++) {
+    newPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    }
+    return(newPassword)
+  }
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -70,7 +71,6 @@ function writePassword() {
   passwordText.value = password;
   console.log(password)
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
