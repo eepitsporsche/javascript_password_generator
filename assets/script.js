fbafac4d682fd276c396dd170f8c91c243fc4043
Porsche <herskorn@gmail.com>
2024-02-19 11:12:16 -0800

    "added generatePassword function and character array"

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,17 @@
+//Function to generate a random password
+function generatePassword() {
 
+  //Character type array for computer to choose from (lowercase, uppercase, numeric, special characters)
+    var numericCharacters = "0123456789";
+    var specialCharacters = "!'()%*+,-.&/:;<=>?@[]^$_`{|#}~";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordCharacters = "";
+    var newPassword = "";
+
+    }
+  
+  
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
