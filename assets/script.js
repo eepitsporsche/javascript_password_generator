dfb20c06c10f066c0b921b508530a94f5b66eb1e
Porsche <herskorn@gmail.com>
2024-02-19 11:12:59 -0800

    "added password length criteria"

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,17 @@ function generatePassword() {
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var passwordCharacters = "";
     var newPassword = "";
+  
+  
+  //User inputs desired password length
+    var userInput = prompt("Enter a password length from 8 to 128.")
+  
+  
+  //If user input is not a number 8-128
+    if (isNaN(userInput) || userInput < 8 || userInput > 128){
+      alert("Please choose a number from 8 to 128.");
+      return
+    }
 
     }
   
